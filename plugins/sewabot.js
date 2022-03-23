2b2177cb5df4c74835492d697f0c5d5050ce9664
BunyanXd <101381544+BunyanXd@users.noreply.github.com>
2022-03-23 11:02:25 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -6,13 +6,13 @@ let handler = async(m, { conn }) => {
 ├ 2 Bulan
 ├ 1 Bulan
 ├ 1 Minggu
-├ Trial 3 Hari
+├ Trial 1 Hari
 └────
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 085811055800
+Dana: 085811055800
 `.trim()
     const button = {
         buttonText: 'List Harga',
